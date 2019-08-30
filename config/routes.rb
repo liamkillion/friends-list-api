29c1c09c1df446f983690bbf68fe6a7d8f5afb8b
Liam Killion <liam.killion@greenhouse.io>
2019-08-30 17:30:13 -0400

    implementing devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :friends, :hangs
+      resources :friends, :hangs, :users
     end
   end
+  root to: "friends#index"
 end
